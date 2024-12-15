d217dc25caa1c34c165d037a86291567f9d229cf
codinghaki <113092980+codinghaki@users.noreply.github.com>
2024-12-15 21:32:00 +1100

    Implement home page header HTML structure

---
+++ src/app/components/homepage-header/homepage-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-header.component.html',
+  styleUrl: './homepage-header.component.css'
+})
+export class HomepageHeaderComponent {
+
+}
