f98e2ed10b09bb2932b2e10be3a5a49385ebf3c4
codinghaki <113092980+codinghaki@users.noreply.github.com>
2024-12-21 20:01:42 +1100

    Implement search

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UnitsService } from '../services/units.service';
+import { UnitInterface } from '../models/unit.interface';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  searchValue = '';
+
+  units: UnitInterface[] = [];
+
+  searchForm: FormGroup;
+
+  constructor(private unitsService: UnitsService, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      searchValue: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.unitsService.getUnits(this.searchValue).subscribe((units) => {
+      this.units = units;
+    });
+  }
+
+  onSearchSubmit(): void {
+    this.searchValue = this.searchForm.value.searchValue ?? '';
+    this.fetchData();
+  }
+}
