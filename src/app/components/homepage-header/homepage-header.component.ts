a7685776d155c51b3bd6105804fbca3d896642f2
codinghaki <113092980+codinghaki@users.noreply.github.com>
2024-12-23 23:31:17 +1100

    Add link back to homepage

--- src/app/components/homepage-header/homepage-header.component.ts
+++ src/app/components/homepage-header/homepage-header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './homepage-header.component.html',
-  styleUrl: './homepage-header.component.css'
+  styleUrl: './homepage-header.component.css',
 })
-export class HomepageHeaderComponent {
-
-}
+export class HomepageHeaderComponent {}
