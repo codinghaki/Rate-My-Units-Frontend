fd633915c3d23e5246548479b3e6e9c69df28ffe
codinghaki <113092980+codinghaki@users.noreply.github.com>
2024-12-31 19:09:36 +1100

    Unrender login/register buttons and render username/logout once user is logged in

--- src/app/components/homepage-header/homepage-header.component.ts
+++ src/app/components/homepage-header/homepage-header.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {AuthService} from "../../services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-homepage-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './homepage-header.component.html',
   styleUrl: './homepage-header.component.css',
 })
-export class HomepageHeaderComponent {
+export class HomepageHeaderComponent implements OnInit{
+  authService = inject(AuthService)
+
   logout(): void {
     console.log('Logout');
+  }
+
+  ngOnInit() {
   }
 }
